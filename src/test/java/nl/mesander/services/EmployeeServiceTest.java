2dcc9b3d82bf9158e1cab227cccfbcd862ff76b1
Mark Mesander <mark_mesander@hotmail.com>
2025-01-13 18:34:53 +0100

    add unit tests for xmlToJava method and add EmployeeTestInvalid.xml

--- src/test/java/nl/mesander/services/EmployeeServiceTest.java
+++ src/test/java/nl/mesander/services/EmployeeServiceTest.java
@@ -4,6 +4,7 @@ package nl.mesander.services;
 import nl.mesander.dtos.input.EmployeeInputDto;
 import nl.mesander.dtos.input.MultipleEmployeeInputDto;
 import nl.mesander.dtos.output.EmployeeDto;
+import nl.mesander.exceptions.IllegalArgumentException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -28,6 +29,7 @@ class EmployeeServiceTest {
     String testFileLocationSuccess;
     String testFileLocationEmpty;
     String testFileLocationNotFound;
+    String testFileLocationInvalid;
 
     String mockJsonStringSuccess;
     String mockJsonStringException;
@@ -65,6 +67,7 @@ class EmployeeServiceTest {
         mockEmployeeInputDtos.add(mockEmployeeInputDto1);
         mockEmployeeInputDtos.add(mockEmployeeInputDto2);
 
+        mockMultipleEmployeeInputDto = new MultipleEmployeeInputDto();
         mockMultipleEmployeeInputDto.setEmployees(mockEmployeeInputDtos);
 
         // Multiple Output Dtos
@@ -76,6 +79,7 @@ class EmployeeServiceTest {
         testFileLocationSuccess = "src/test/resources/EmployeeTest.xml";
         testFileLocationEmpty = "";
         testFileLocationNotFound = "/src/test/resources/NotHere.xml";
+        testFileLocationInvalid = "src/test/resources/EmployeeTestInvalid.xml";
 
         //Json
         mockJsonStringSuccess = "{\"name\":\"Mark Super Test\",\"function\":\"Medior Java Developer Test\",\"company\":\"Copernicus Super Test\",\"isHired\":\"100% Yes\"}";
@@ -99,60 +103,96 @@ class EmployeeServiceTest {
         mockJsonStringException = "";
     }
 
+    // Transfer Method Tests
     @Test
     @DisplayName("Should transfer EmployeeInputDto to EmployeeDto")
     void employeeToDto_Success() {
         // Arrange
-        // BeforeEach init EmployeeInputDto: mockEmployeeInputDto
+        // BeforeEach init EmployeeInputDto: mockEmployeeInputDto1
 
         // Act
-        EmployeeDto result = EmployeeService.employeeToDto(mockEmployeeInputDto);
+        EmployeeDto result = EmployeeService.employeeToDto(mockEmployeeInputDto1);
 
         // Assert
-        assertEquals(mockEmployeeInputDto.getName(), result.getName());
-        assertEquals(mockEmployeeInputDto.getFunction(), result.getFunction());
-        assertEquals(mockEmployeeInputDto.getCompany(), result.getCompany());
-        assertEquals(mockEmployeeInputDto.getToHire(), result.getIsHired());
+        assertEquals(mockEmployeeInputDto1.getName(), result.getName());
+        assertEquals(mockEmployeeInputDto1.getFunction(), result.getFunction());
+        assertEquals(mockEmployeeInputDto1.getCompany(), result.getCompany());
+        assertEquals(mockEmployeeInputDto1.getToHire(), result.getIsHired());
     }
 
+    // Transfer XML to Java Tests
     @Test
     @DisplayName("Should transfer Xml file to MultipleEmployeeInputDto")
     void XmlToJava_Success() {
         // Arrange
+        // BeforeEach init String: testFileLocationSuccess
 
         // Act
+        MultipleEmployeeInputDto result = EmployeeService.xmlToJava(testFileLocationSuccess);
 
         // Assert
+        assertEquals("Mark Mesander Test", result.getEmployees().get(0).getName());
+        assertEquals("Medior Java Developer", result.getEmployees().get(0).getFunction());
+        assertEquals("Copernicus", result.getEmployees().get(0).getCompany());
+        assertEquals("Absolutely", result.getEmployees().get(0).getToHire());
+        assertEquals("David Verloop", result.getEmployees().get(1).getName());
+        assertEquals("Tech Lead", result.getEmployees().get(1).getFunction());
+        assertEquals("Copernicus", result.getEmployees().get(1).getCompany());
+        assertEquals("Already Hired", result.getEmployees().get(1).getToHire());
     }
 
     @Test
     @DisplayName("Should throw exception: file location is null/empty")
     void XmlToJava_Exception_WhenEmpty() {
         // Arrange
+        // BeforeEach init String: testFileLocationEmpty
 
         // Act
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            EmployeeService.xmlToJava(testFileLocationEmpty);
+        });
 
         // Assert
+        String expectedMessage = "File location is null/empty";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
     }
 
     @Test
     @DisplayName("Should throw exception: File doesn't exist")
     void testXmlToJava_Exception_WhenDoesNotExist() {
         // Arrange
+        // BeforeEach init String: testFileLocationNotFound
 
         // Act
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            EmployeeService.xmlToJava(testFileLocationNotFound);
+        });
+
+        String expectedMessage = "File: " + testFileLocationNotFound + " doesn't exist";
+        String actualMessage = exception.getMessage();
 
         // Assert
+        assertEquals(expectedMessage, actualMessage);
     }
 
     @Test
     @DisplayName("Should throw exception: Problem with unmarshalling file")
     void testXmlToJava_Exception_WhenJAXBException() {
         // Arrange
+        // BeforeEach init String: testFileLocationInvalid
 
         // Act
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            EmployeeService.xmlToJava(testFileLocationInvalid);
+        });
 
         // Assert
+        String expectedMessage = "Problem with unmarshalling file " + testFileLocationInvalid + ": ";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage), "Expected message should contain actual message");
     }
 
     //    @Test
