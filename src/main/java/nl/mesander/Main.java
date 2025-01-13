65b022f661cce74dd427568b87b06e8cdf50834d
Mark Mesander <mark_mesander@hotmail.com>
2025-01-13 16:33:57 +0100

    add MultipleEmployeeDto for output java

--- src/main/java/nl/mesander/Main.java
+++ src/main/java/nl/mesander/Main.java
@@ -30,6 +30,8 @@ public class Main {
 
 
 
+
+
 //
 //        if (employeeInputDto != null) {
 //            employeeDto = employeeToDto(employeeInputDto);
---
+++ src/main/java/nl/mesander/dtos/output/MultipleEmployeeDto.java
@@ -0,0 +1,13 @@
+package nl.mesander.dtos.output;
+
+// Imports
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class MultipleEmployeeDto {
+    private List<EmployeeDto> employeeDtos;
+}
