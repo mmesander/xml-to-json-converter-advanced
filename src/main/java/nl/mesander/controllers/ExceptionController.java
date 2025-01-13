66179d483447b7be47db8506bdb0f3a48c942010
Mark Mesander <mark_mesander@hotmail.com>
2025-01-13 16:04:20 +0100

    add exception controller

--- src/main/java/nl/mesander/controllers/ExceptionController.java
+++ src/main/java/nl/mesander/controllers/ExceptionController.java
@@ -1,4 +1,14 @@
 package nl.mesander.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class ExceptionController {
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
